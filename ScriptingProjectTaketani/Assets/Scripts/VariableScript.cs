82343ab41703d7872129eb801c3ced455b551748
Alexander T <719277@student.dusd.net>
2023-10-26 15:03:27 -0700

    Made Variable Script
    made new variable script

---
+++ ScriptingProjectTaketani/Assets/Scripts/VariableScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class VariableScript : MonoBehaviour
+{
+    public int myInt = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int MultiplyByTwo (int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
